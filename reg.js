bc804b91aaed07ec3aa0d36da3f7911b5bef3169
Anwar <144898207+Anwarahmedsh@users.noreply.github.com>
2023-11-26 14:53:33 +0300

    saras

--- reg.js
+++ reg.js
@@ -69,8 +69,7 @@ function validateForm() {
   }
 
 // Store child's name in local storage
-var childInfo = { name: name };
-localStorage.setItem('childName', name);
+var childInfo = { name: name, image: "path_to_image", age: 10, interests: "Interest" };
 
 // Check if there are already saved children in local storage
 let parentRegisteredKids = JSON.parse(localStorage.getItem("parentRegisteredKids")) || [];
@@ -80,6 +79,6 @@ parentRegisteredKids.push(childInfo);
 
 // Update local storage for registered kids
 localStorage.setItem("parentRegisteredKids", JSON.stringify(parentRegisteredKids));
-  return true; // Proceed with form submission
+
 }
 
